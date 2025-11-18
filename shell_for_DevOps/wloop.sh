59b0c60e023715bf1973e3e11b8995aff1a4f3d6
KiranKolekar28 <kolekarkiran1234@gmail.com>
2025-11-18 19:48:53 +0530

    we have added wloop.sh

---
+++ shell_for_DevOps/wloop.sh
@@ -0,0 +1,47 @@
+
+#!/bin/bash
+
+<<dddd
+num=0
+
+while [[ $num -le 5 ]]
+do 
+	echo "heelo"
+	num=$num+1
+done
+dddd
+
+<<cd
+
+num=0
+while [[ $num -le 5 ]]
+do
+	echo " hello"
+	num=$num+1
+done
+
+cd
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
